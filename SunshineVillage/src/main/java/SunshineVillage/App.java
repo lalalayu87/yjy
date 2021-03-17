bb3b340e6e84edc5f0abd76c72b3f68b1de097f4
DESKTOP-78MNPFP\lalal <lalalayu87@gmail.com>
2021-03-17 16:49:22 +0900

    프로젝트 시작

---
+++ SunshineVillage/src/main/java/SunshineVillage/App.java
@@ -0,0 +1,18 @@
+package SunshineVillage;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages= {"SunshineVillage"})
+
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+
+	}
+
+}
