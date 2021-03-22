6ce1745c985daacb235493ea5e2721489d736f05
DESKTOP-78MNPFP\lalal <lalalayu87@gmail.com>
2021-03-22 15:18:42 +0900

    SPRING (prefix, suffix)

--- webProject/src/main/java/webProject/IndexController.java
+++ webProject/src/main/java/webProject/IndexController.java
@@ -5,18 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 public class IndexController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		//2. handleRequest가 실행됨
-		System.out.println("index controller");
 		
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("data", "Hello Spring MVC~");
-		mv.setViewName("/WEB-INF/view/index.jsp");
+		//mv.setViewName("/WEB-INF/view/index.jsp");
 		//ModelAndView를 data라는 이름으로 담아서 는 "index.jsp"에 전달
+		
 		
 		return mv;
 	}
