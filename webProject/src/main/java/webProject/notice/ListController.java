81f455e1e7f67a4a636194458babbaa74bc7b0c0
DESKTOP-78MNPFP\lalal <lalalayu87@gmail.com>
2021-03-22 18:31:58 +0900

    SPRING (CSS 연습중)

---
+++ webProject/src/main/java/webProject/notice/ListController.java
@@ -0,0 +1,22 @@
+package webProject.notice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ListController implements Controller{
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//2. handleRequest가 실행됨
+		
+		ModelAndView mv = new ModelAndView("notice.list");
+		//mv.setViewName("/WEB-INF/view/notice/index.jsp");
+		
+		
+		return mv;
+	}
+
+
+}
